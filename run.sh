1a45bf0c41f72f265bacb1180294bdd1ba199766
Harun Korkmaz <58360416+karunland@users.noreply.github.com>
2024-06-09 18:03:54 +0300

    update

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source myenv/bin/activate
-python3 app.py
--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt install python3.11-venv
-python3 -m venv myenv
-source myenv/bin/activate
-pip3 install -r requirements.txt 
-deactivate
